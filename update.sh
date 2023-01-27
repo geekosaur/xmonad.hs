f460acb14ef966590036d97792e7f515ee681051
brandon s allbery kf8nh <allbery.b@gmail.com>
2023-01-27 11:37:14 -0500

    verbose; also, install xmonad before using it

--- update.sh
+++ update.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 echo "Go update github xmonad and xmonad-contrib; I'll wait…"
 read dummy
+echo '$ cabal update'
 cabal update
 for pkg in X11 X11-xft xmonad xmonad-contrib; do
+  echo '$ (cd "$pkg"; git pull)'
   (cd "$pkg"; git pull)
 done
+echo '$ cabal install xmonad --overwrite-policy-always'
+cabal install xmonad --overwrite-policy=always
+hash -r
+echo '$ xmonad --recompile'
 xmonad --recompile
+echo '$ echo :q | cabal repl'
 echo :q | cabal repl
 # xmonad-log-applet original repo is gone; there's a new repo
 # but it's fairly minimal-looking (but this might be a good
