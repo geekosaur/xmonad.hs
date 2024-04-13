ad5f6d226f54c9b67396d86e6e9ca52637422e51
brandon s allbery kf8nh <allbery.b@gmail.com>
2024-04-12 22:29:51 -0400

    migrate old multimonitor screenconfig

--- arandr-hilfy.sh
+++ arandr-hilfy.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-xrandr --output eDP --primary --mode 2256x1504 --pos 2560x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+count=$(xrandr --listactivemonitors | sed '1{;s/Monitors: //;q;}')
+if [ "$count" -eq 1 ]; then
+    xrandr --output eDP --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+else
+    xrandr --output eDP --primary --mode 2256x1504 --pos 2560x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+fi
