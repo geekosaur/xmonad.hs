bf6f9235d3925c049ec63d604fe0c71d70959391
brandon s allbery kf8nh <allbery.b@gmail.com>
2025-01-05 15:23:36 -0500

    maybe got it right-ish this time?
    also, upgraded laptop display with higher resolution

--- arandr-hilfy.sh
+++ arandr-hilfy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+exec >>/tmp/screenlayout.dbg 2>&1
+printf "--------\n"
 count=$(xrandr --listactivemonitors | sed '1{;s/Monitors: //;q;}')
 if [ "$count" -eq 1 ]; then
-    xrandr --output eDP --primary --mode 2256x1504 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
+    xrandr --output eDP --primary --mode 2880x1920 --pos 0x0 --rotate normal --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --off --output DisplayPort-7 --off
     exit 0
 fi
 
@@ -15,16 +17,27 @@ fi
 # (Note that all the non-built-in displays are always exposed to X11 as if
 # they are DisplayPort, but are actually USB3 on the laptop end.)
 # Note that one of the outputs may be DisplayPort-0 or DisplayPort-1!
+#
+# We also need to extract the preferred mode for each display, because
+# they all get downgraded to match the stupidity of the above-mentioned
+# 1080p-default monitor. So we need to extract the mode of the one marked
+# with `+`. (But see the later comment.)
+#
+# NOTE: At some point the DisplayPort port on the second external monitor
+# did whatever DisplayPorts always do for me (hardware failure), so that
+# is no longer connected. The monitor still starts at 1080p, but can be
+# upgraded successfully now without the DP being connected. (Don't ask me.)
+#
 
 panels="$(xrandr | sed -e 1d -e '/^[^ ]/!d' -e '/ disconnected /d' -e 's/ .*$//' -e '/^eDP$/d' -e '/^DisplayPort-2$/d' | tr '\n' ' ')"
-panels="${panels/ *}" # strictly speaking there should be only one left
+#panels="${panels/ *}" # strictly speaking there should be only one left
 dps="$(xrandr | sed -e 1d -e '/^[^ ]/!d' -e 's/ .*$//' -e '/^eDP$/d' | tr '\n' ' ')"
 
 randr=
 for output in $dps; do
     case " $panels " in
-    *" $output "*)
-        randr="$randr --output $output --right-of eDP"
+	*" $output "*)
+        randr="$randr --output $output --right-of eDP --mode 2560x1440"
         ;;
     *)
         randr="$randr --output $output --off"
@@ -33,9 +46,11 @@ for output in $dps; do
 done
 
 # note for if xrandr screws up again and drops the whole thing to 1080p:
-# eDP: --mode 2256x1504
+# eDP: --mode 2880x1920
 # DisplayPort-2: --mode 2560x1440
 # <random>: --mode 2560x1440
 
-# shellcheck disable=SC2086
-xrandr --output eDP --primary --output DisplayPort-2 --left-of eDP $randr
+xrandr
+doit="xrandr --output eDP --primary --mode 2880x1920 --output DisplayPort-2 --left-of eDP --mode 2560x1440 $randr"
+echo "$doit"
+eval "$doit"
